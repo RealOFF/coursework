c9f573d42e14782d88a63e023a6921cd0800a3f8
RealOFF <gimpelllll@gmail.com>
2020-11-11 22:34:57 +0300

    Delete user module

--- src/modules/baseRouter.ts
+++ src/modules/baseRouter.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { IRouter } from './router.interface';
 import { StudentRouter } from './student/student.router';
-import userRouter from './user/userRouter';
 
 // Init router
 export const router = Router();
@@ -15,7 +14,6 @@ export class BaseRouter implements IRouter {
 	// eslint-disable-line
 	get routes() {
 		router.use('/students', this.studentRouter.routes);
-		router.use('/users', userRouter.routes);
 		return router;
 	}
 }
