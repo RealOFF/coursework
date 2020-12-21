5d0458da120ef7ce37b18ef2c32d648bd0d5a0aa
RealOFF <gimpelllll@gmail.com>
2020-12-22 01:37:05 +0300

    Add role constant

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,3 +3,8 @@ export const SUBJECT_SESSIONS_STATUSES = {
 	IN_PROGRES: 'in_progres',
 	FINISHED: 'finished',
 };
+
+export const USER_ROLES = {
+    ADMIN: 'admin',
+    COMMON_USER: 'common_user',
+};
